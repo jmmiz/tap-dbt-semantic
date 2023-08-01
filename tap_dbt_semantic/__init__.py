8f898837f1eebfc0134ebd8cff8681d6c4bd93ad
John Michael Mizerany <jmizerany3@gatech.edu>
2023-07-31 19:56:55 -0500

    create using sdk

---
+++ tap_dbt_semantic/__init__.py
@@ -0,0 +1,1 @@
+"""Tap for dbt-semantic."""
---
+++ tap_dbt_semantic/client.py
@@ -0,0 +1,62 @@
+"""GraphQL client handling, including dbt-semanticStream base class."""
+
+from __future__ import annotations
+
+from typing import Iterable
+
+import requests  # noqa: TCH002
+from singer_sdk.streams import GraphQLStream
+
+
+class dbt-semanticStream(GraphQLStream):
+    """dbt-semantic stream class."""
+
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        # TODO: hardcode a value here, or retrieve it from self.config
+        return "https://api.mysample.com"
+
+    @property
+    def http_headers(self) -> dict:
+        """Return the http headers needed.
+
+        Returns:
+            A dictionary of HTTP headers.
+        """
+        headers = {}
+        if "user_agent" in self.config:
+            headers["User-Agent"] = self.config.get("user_agent")
+        # If not using an authenticator, you may also provide inline auth headers:
+        # headers["Private-Token"] = self.config.get("auth_token")
+        return headers
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+
+        Yields:
+            Each record from the source.
+        """
+        # TODO: Parse response body and return a set of records.
+        resp_json = response.json()
+        yield from resp_json.get("<TODO>")
+
+    def post_process(
+        self,
+        row: dict,
+        context: dict | None = None,  # noqa: ARG002
+    ) -> dict | None:
+        """As needed, append or transform raw data to match expected structure.
+
+        Args:
+            row: An individual record from the stream.
+            context: The stream context.
+
+        Returns:
+            The updated record dictionary, or ``None`` to skip the record.
+        """
+        # TODO: Delete this method if not needed.
+        return row
